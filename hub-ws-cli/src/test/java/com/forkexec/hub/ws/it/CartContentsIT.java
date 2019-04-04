eb9bd7ca5fa1abf1eca83d4fb586fdff810ee4d7
MVBarros <miguel.v.barros@tecnico.ulisboa.pt>
2019-04-05 00:47:50 +0100

    Minor fixes

---
+++ hub-ws-cli/src/test/java/com/forkexec/hub/ws/it/CartContentsIT.java
@@ -0,0 +1,71 @@
+package com.forkexec.hub.ws.it;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+
+import com.forkexec.hub.ws.Food;
+import com.forkexec.hub.ws.FoodId;
+import com.forkexec.hub.ws.FoodInit;
+import com.forkexec.hub.ws.FoodOrder;
+import com.forkexec.hub.ws.FoodOrderItem;
+import com.forkexec.hub.ws.InvalidFoodIdFault_Exception;
+import com.forkexec.hub.ws.InvalidFoodQuantityFault_Exception;
+import com.forkexec.hub.ws.InvalidInitFault_Exception;
+import com.forkexec.hub.ws.InvalidUserIdFault_Exception;
+
+public class CartContentsIT extends BaseIT{
+	
+	@BeforeClass
+	public void oneTimeSetUp() throws InvalidInitFault_Exception, InvalidUserIdFault_Exception, InvalidFoodIdFault_Exception, InvalidFoodQuantityFault_Exception {
+		client.ctrlClear();
+
+		
+		// First foodinit
+		FoodId foodid1 = new FoodId();
+		foodid1.setMenuId("1");
+		foodid1.setRestaurantId("T08_Restaurant1");
+		
+		Food f1 = new Food();
+		f1.setId(foodid1);
+		f1.setEntree("Salada de Polvo");
+		f1.setPlate("Arroz");
+		f1.setDessert("Bolo");
+		f1.setPrice(15);
+		f1.setPreparationTime(3);
+		
+		FoodInit foodinit1 = new FoodInit();
+		foodinit1.setFood(f1);
+		foodinit1.setQuantity(2);
+		
+		
+		
+		List<FoodInit> foodInits = new ArrayList<>(Arrays.asList(foodinit1));
+		client.ctrlInitFood(foodInits);
+		
+		client.activateAccount("teste@mail.com");
+		client.addFoodToCart("ola@ola.com", foodid1, 1);
+		
+		client.activateAccount("teste2@mail.com");
+
+	}
+	
+	@AfterClass
+	public void oneTimeTearDown() {
+		client.ctrlClear();
+	}
+	
+	public void sucesssCartContents() throws InvalidUserIdFault_Exception {
+		
+		List<FoodOrderItem> contents = client.cartContents("ola@ola.com");
+		
+		assertEquals(contents.size(), 1);
+		
+	}
+
+}
--- hub-ws-cli/src/test/java/com/forkexec/hub/ws/it/loadAccountIT.java
+++ hub-ws-cli/src/test/java/com/forkexec/hub/ws/it/loadAccountIT.java
@@ -14,69 +14,110 @@ import com.forkexec.hub.ws.InvalidMoneyFault_Exception;
 import com.forkexec.hub.ws.InvalidUserIdFault_Exception;
 
 public class loadAccountIT extends BaseIT {
-	
+
 	@Before
 	public void setUp() throws InvalidUserIdFault_Exception {
 		client.activateAccount("ola@ola.com");
 	}
-	
+
 	@After
 	public void tearDown() {
 		// clear remote service state after all tests
 		client.ctrlClear();
 	}
-	
-	//Main Test
+
+	// Main Test
 	@Test
-	public void sucessLoadAccount() throws InvalidCreditCardFault_Exception, InvalidMoneyFault_Exception, InvalidUserIdFault_Exception {
+	public void sucessLoadAccountwith10()
+			throws InvalidCreditCardFault_Exception, InvalidMoneyFault_Exception, InvalidUserIdFault_Exception {
 		client.loadAccount("ola@ola.com", 10, "4024007102923926");
-		
+
 		assertEquals(client.accountBalance("ola@ola.com"), 1100);
 	}
-	
-	//badInputTests
-	@Test(expected = InvalidUserIdFault_Exception.class )
-	public void nullEmailLoadAccount() throws InvalidCreditCardFault_Exception, InvalidMoneyFault_Exception, InvalidUserIdFault_Exception {
-		client.loadAccount(null, 10, "4024007102923926");
-		
+
+	@Test
+	public void sucessLoadAccountwith20()
+			throws InvalidCreditCardFault_Exception, InvalidMoneyFault_Exception, InvalidUserIdFault_Exception {
+		client.loadAccount("ola@ola.com", 20, "4024007102923926");
+
+		assertEquals(client.accountBalance("ola@ola.com"), 2200);
 	}
+
+	@Test
+	public void sucessLoadAccountwith30()
+			throws InvalidCreditCardFault_Exception, InvalidMoneyFault_Exception, InvalidUserIdFault_Exception {
+		client.loadAccount("ola@ola.com", 30, "4024007102923926");
+
+		assertEquals(client.accountBalance("ola@ola.com"), 3400);
+	}
+
+	@Test
+	public void sucessLoadAccountwith50()
+			throws InvalidCreditCardFault_Exception, InvalidMoneyFault_Exception, InvalidUserIdFault_Exception {
+		client.loadAccount("ola@ola.com", 50, "4024007102923926");
+
+		assertEquals(client.accountBalance("ola@ola.com"), 5600);
+	}
+
+	// badInputTests
 	@Test(expected = InvalidUserIdFault_Exception.class)
-	public void emptyEmailLoadAccount() throws InvalidCreditCardFault_Exception, InvalidMoneyFault_Exception, InvalidUserIdFault_Exception {
+	public void nullEmailLoadAccount()
+			throws InvalidCreditCardFault_Exception, InvalidMoneyFault_Exception, InvalidUserIdFault_Exception {
+		client.loadAccount(null, 10, "4024007102923926");
+
+	}
+
+	@Test(expected = InvalidUserIdFault_Exception.class)
+	public void emptyEmailLoadAccount()
+			throws InvalidCreditCardFault_Exception, InvalidMoneyFault_Exception, InvalidUserIdFault_Exception {
 		client.loadAccount("", 10, "4024007102923926");
 	}
-	
+
 	@Test(expected = InvalidUserIdFault_Exception.class)
-	public void newlineLoadAccount() throws InvalidCreditCardFault_Exception, InvalidMoneyFault_Exception, InvalidUserIdFault_Exception {
+	public void newlineLoadAccount()
+			throws InvalidCreditCardFault_Exception, InvalidMoneyFault_Exception, InvalidUserIdFault_Exception {
 		client.loadAccount("\n", 10, "4024007102923926");
 	}
-	
+
 	@Test(expected = InvalidUserIdFault_Exception.class)
-	public void tablineLoadAccount() throws InvalidCreditCardFault_Exception, InvalidMoneyFault_Exception, InvalidUserIdFault_Exception {
+	public void tablineLoadAccount()
+			throws InvalidCreditCardFault_Exception, InvalidMoneyFault_Exception, InvalidUserIdFault_Exception {
 		client.loadAccount("\t", 10, "4024007102923926");
 	}
-	
+
 	@Test(expected = InvalidUserIdFault_Exception.class)
-	public void noSuchEmailLoadAccount() throws InvalidCreditCardFault_Exception, InvalidMoneyFault_Exception, InvalidUserIdFault_Exception {
+	public void noSuchEmailLoadAccount()
+			throws InvalidCreditCardFault_Exception, InvalidMoneyFault_Exception, InvalidUserIdFault_Exception {
 		client.loadAccount("ola2@ola.com", 10, "4024007102923926");
 	}
-	
+
 	@Test(expected = InvalidMoneyFault_Exception.class)
-	public void negativePointsLoadAccount() throws InvalidCreditCardFault_Exception, InvalidMoneyFault_Exception, InvalidUserIdFault_Exception {
+	public void negativePointsLoadAccount()
+			throws InvalidCreditCardFault_Exception, InvalidMoneyFault_Exception, InvalidUserIdFault_Exception {
 		client.loadAccount("ola@ola.com", -10, "4024007102923926");
 	}
-	
+
+	@Test(expected = InvalidMoneyFault_Exception.class)
+	public void nonValidPointsLoadAccount()
+			throws InvalidCreditCardFault_Exception, InvalidMoneyFault_Exception, InvalidUserIdFault_Exception {
+		client.loadAccount("ola@ola.com", 15, "4024007102923926");
+	}
+
 	@Test(expected = InvalidCreditCardFault_Exception.class)
-	public void invalidCreditCardLoadAccount() throws InvalidCreditCardFault_Exception, InvalidMoneyFault_Exception, InvalidUserIdFault_Exception {
+	public void invalidCreditCardLoadAccount()
+			throws InvalidCreditCardFault_Exception, InvalidMoneyFault_Exception, InvalidUserIdFault_Exception {
 		client.loadAccount("ola@ola.com", 10, "4");
 	}
-	
+
 	@Test(expected = InvalidCreditCardFault_Exception.class)
-	public void nullCreditCardLoadAccount() throws InvalidCreditCardFault_Exception, InvalidMoneyFault_Exception, InvalidUserIdFault_Exception {
+	public void nullCreditCardLoadAccount()
+			throws InvalidCreditCardFault_Exception, InvalidMoneyFault_Exception, InvalidUserIdFault_Exception {
 		client.loadAccount("ola@ola.com", 10, null);
 	}
-	
+
 	@Test(expected = InvalidCreditCardFault_Exception.class)
-	public void emptyCreditCardLoadAccount() throws InvalidCreditCardFault_Exception, InvalidMoneyFault_Exception, InvalidUserIdFault_Exception {
+	public void emptyCreditCardLoadAccount()
+			throws InvalidCreditCardFault_Exception, InvalidMoneyFault_Exception, InvalidUserIdFault_Exception {
 		client.loadAccount("ola@ola.com", 10, "");
 	}
 }
