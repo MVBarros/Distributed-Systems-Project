8e75b9bb3137a55d38f94c19eea05fc74451799f
Carolina Carreira <carolina.carreira@tecnico.ulisboa.pt>
2019-04-04 23:29:36 +0100

    Started SearchDealIT

---
+++ hub-ws-cli/src/test/java/com/forkexec/hub/ws/it/SearchDealIT.java
@@ -0,0 +1,109 @@
+package com.forkexec.hub.ws.it;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.forkexec.hub.ws.Food;
+import com.forkexec.hub.ws.FoodId;
+import com.forkexec.hub.ws.FoodInit;
+import com.forkexec.hub.ws.InvalidFoodIdFault_Exception;
+import com.forkexec.hub.ws.InvalidInitFault_Exception;
+import com.forkexec.hub.ws.InvalidTextFault_Exception;
+
+public class SearchDealIT extends BaseIT {
+
+	// one-time initialization and clean-up
+	@BeforeClass
+	public static void oneTimeSetUp() throws InvalidInitFault_Exception {
+		client.ctrlClear();
+
+		
+		// First foodinit
+		FoodId foodid1 = new FoodId();
+		foodid1.setMenuId("1");
+		foodid1.setRestaurantId("T08_Restaurant1");
+		
+		Food f1 = new Food();
+		f1.setId(foodid1);
+		f1.setEntree("Salada de Polvo");
+		f1.setPlate("Arroz");
+		f1.setDessert("Bolo");
+		f1.setPrice(15);
+		f1.setPreparationTime(3);
+		
+		FoodInit foodinit1 = new FoodInit();
+		foodinit1.setFood(f1);
+		foodinit1.setQuantity(2);
+		
+		//Second foodinit
+		FoodId foodid2 = new FoodId();
+		foodid2.setMenuId("2");
+		foodid2.setRestaurantId("T08_Restaurant1");
+		
+		Food f2 = new Food();
+		f2.setId(foodid2);
+		f2.setEntree("Salada de Polvo");
+		f2.setPlate("Arroz");
+		f2.setDessert("Bolo");
+		f2.setPrice(15);
+		f2.setPreparationTime(4);
+		
+		FoodInit foodinit2 = new FoodInit();
+		foodinit2.setFood(f2);
+		foodinit2.setQuantity(2);
+		
+		
+		
+		List<FoodInit> foodInits = new ArrayList<>(Arrays.asList(foodinit1, foodinit2));
+		client.ctrlInitFood(foodInits);
+
+		
+	}
+	
+	@AfterClass
+	public static void oneTimeTearDown() {
+		// clear remote service state after all tests
+		client.ctrlClear();
+
+
+	}
+	
+	@Before
+	public void setUp()  {
+		
+
+	}
+
+	@After
+	public void tearDown() {
+		client.ctrlClear();
+
+
+	}
+	
+	//Bad Input Tests
+	@Test(expected =InvalidTextFault_Exception.class)
+	public void searchDealNullDescription() throws InvalidTextFault_Exception{
+		client.searchDeal(null);
+	}
+	
+	@Test(expected =InvalidTextFault_Exception.class)
+	public void searchDealEmptyDescription() throws InvalidTextFault_Exception{
+		client.searchDeal("");
+	}
+	
+	@Test(expected =InvalidTextFault_Exception.class)
+	public void searchDealWhiteSpaceDescription() throws InvalidTextFault_Exception{
+		client.searchDeal(" ");
+	}
+	
+}
