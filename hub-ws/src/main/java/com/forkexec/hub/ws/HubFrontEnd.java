f7e4e9268b97460f5313904746622e8c3cdfdce8
Miguel Veloso Barros <ist187691@i5-008.tagus.ist.utl.pt>
2019-05-01 20:51:00 +0100

    implement FrontEndConstructor

--- hub-ws/src/main/java/com/forkexec/hub/ws/HubFrontEnd.java
+++ hub-ws/src/main/java/com/forkexec/hub/ws/HubFrontEnd.java
@@ -1,5 +1,17 @@
 package com.forkexec.hub.ws;
 
+import java.util.Collection;
+
+import com.forkexec.pts.ws.cli.PointsClient;
+
 public class HubFrontEnd {
+	private Collection<PointsClient> clients;
+	private int quorumSize;
+	
+	public HubFrontEnd(Collection<PointsClient> clients) {
+		this.clients = clients;
+		this.quorumSize = clients.size() / 2 + 1;
+	}
+	
 	
 }
