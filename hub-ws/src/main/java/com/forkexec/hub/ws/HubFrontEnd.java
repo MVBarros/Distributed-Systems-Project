0a30a1f2324806a7c5fcf2caed7893e0778f7df4
Carolina Carreira <carolina.carreira@tecnico.ulisboa.pt>
2019-05-01 21:10:40 +0100

    started assync

--- hub-ws/src/main/java/com/forkexec/hub/ws/HubFrontEnd.java
+++ hub-ws/src/main/java/com/forkexec/hub/ws/HubFrontEnd.java
@@ -2,6 +2,9 @@ package com.forkexec.hub.ws;
 
 import java.util.Collection;
 
+import javax.xml.ws.AsyncHandler;
+import javax.xml.ws.Response;
+
 import com.forkexec.pts.ws.cli.PointsClient;
 
 public class HubFrontEnd {
@@ -11,6 +14,21 @@ public class HubFrontEnd {
 	public HubFrontEnd(Collection<PointsClient> clients) {
 		this.clients = clients;
 		this.quorumSize = clients.size() / 2 + 1;
+	}
+	
+	public int pointsReadAsync(String email) {
+		for (PointsClient client: clients) {
+			client.pointsReadAsync(email, new AsyncHandler<PointsReadResponse>() {
+				@Override
+		        public void handleResponse(Response<PointsReadResponse> response) {
+					
+				}
+				
+		});
+			
+		
+		
+		}
 	}
 	
 	
